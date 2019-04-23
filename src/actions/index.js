3a73a9ebc27b5ac2411e1422cfa67403c4fc4619
AmirolAhmad <dev.amirol@gmail.com>
2019-04-23 16:33:44 +0800

    bulk action creator for stream crud

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM } from "./types";
 
+// User authentication with Google API
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
@@ -14,6 +15,7 @@ export const signOut = () => {
   };
 };
 
+// Stream CRUD
 export const createStream = formValues => async dispatch => {
   const response = await streams.post('/streams', formValues);
   dispatch({
@@ -21,3 +23,35 @@ export const createStream = formValues => async dispatch => {
     payload: response.data
   });
 };
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: response.data
+  });
+}
+
+export const fetchStream = (id) => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({
+    type: FETCH_STREAM,
+    payload: response.data
+  });
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({
+    type: EDIT_STREAM,
+    payload: response.data
+  });
+}
+
+export const deleteStream = (id) => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({
+    type: DELETE_STREAM,
+    payload: id
+  });
+}
