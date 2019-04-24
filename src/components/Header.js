50e8ff8b30491fbf8915a20a4bc5a37315e2891b
AmirolAhmad <dev.amirol@gmail.com>
2019-04-24 13:51:19 +0800

    change stream name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import GoogleAuth from './GoogleAuth';
 const Header = () => {
   return(
     <div className="ui secondary pointing menu">
-      <Link to="/" className="item">Streamer</Link>
+      <Link to="/" className="item">MY Streamer</Link>
       <div className="right menu">
         <Link to="/" className="item">All Streams</Link>
         <div className="item">
