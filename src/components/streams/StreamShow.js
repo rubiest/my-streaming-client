7a1ce813eefae74c8efad7145898d7c8a41bf41c
AmirolAhmad <dev.amirol@gmail.com>
2019-04-24 16:30:28 +0800

    showing stream live with flv

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -19,9 +52,25 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
 
     return (
-      <div>
-        <h1>{title}</h1>
-        <h5>{description}</h5>
+      <div className="ui piled segment">
+        <video
+          ref={this.videoRef}
+          style={{ width: "100%" }}
+          controls={true}
+        />
+        <div className="ui relaxed divided list">
+          <div className="item">
+            <Link to="/" className="ui right floated content button">
+              Back to list
+            </Link>
+            <div class="content">
+              <div className="ui large header">
+                <h1>{title}</h1>
+              </div>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
