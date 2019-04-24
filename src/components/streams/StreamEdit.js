cb4c8380e32f91ed7928a7a8457070760ff24458
AmirolAhmad <dev.amirol@gmail.com>
2019-04-24 12:35:29 +0800

    edit form submission

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,45 @@
+import _ from 'lodash';
 import React from 'react';
+import { connect } from "react-redux";
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from "./StreamForm";
 
-const StreamEdit = () => {
-  return (
-    <div>StreamEdit</div>
-  )
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return (
+        <div className="ui active inverted dimmer">
+          <div className="ui text loader">Loading</div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="ui piled segment">
+        <div className="ui inverted segment">
+          <h4 className="ui header">Edit Stream Video</h4>
+        </div>
+        <div className="ui segment">
+          <StreamForm
+            initialValues={_.pick(this.props.stream, 'title', 'description')}
+            onSubmit={this.onSubmit}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
